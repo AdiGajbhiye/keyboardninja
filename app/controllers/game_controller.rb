468d2ca932a9752dec10a9defd6ae510109c21f4
Shikher <root@shikherverma.com>
2016-10-12 21:33:56 +0530

    Add game controller

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,19 @@
+class GameController < ApplicationController
+    def new
+    end
+
+    def join
+    end
+
+    def delete
+    end
+
+    def getStatus
+    end
+
+    def postPosition
+    end
+
+    def getResult
+    end
+end
