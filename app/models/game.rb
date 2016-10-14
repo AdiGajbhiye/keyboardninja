c2c780c324adae7be32def24d0a51e8832b37ca2
Shikher <root@shikherverma.com>
2016-10-15 05:13:18 +0530

    Move model specific behaviour to game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,16 +1,36 @@
 class Game < ApplicationRecord
-  GAME_DURATION = 1.minutes
   has_many :players
   serialize :wordsArray,Array
 
   def current?
     now = DateTime.now
-    created_at < now && now < (created_at + GAME_DURATION)
+    created_at < now && now < (created_at + KeyboardNinja::GAME_DURATION)
   end
 
   def finished?
     now = DateTime.now
-    now > (created_at + GAME_DURATION)
+    now > (created_at + KeyboardNinja::GAME_DURATION)
   end
 
+  def status
+    if current?
+      array = players.collect do |player|
+        hash = { :name => player.name, :position => player.position, :errors => player.mistakesArray.size } 
+      end
+    else
+      raise KeyboardNinja::HTTP_FORBIDDEN
+    end
+  end
+
+  def result
+    if finished?
+      array = players.collect do |player|
+        total_words_typed = player.position + 1
+        player.wpm =  ( total_words_typed - player.mistakesArray.size ) / KeyboardNinja::GAME_DURATION
+        hash = { :name => player.name, :position => player.position, :errors => player.mistakesArray.size, :wpm => player.wpm } 
+      end
+    else
+      raise KeyboardNinja::HTTP_FORBIDDEN
+    end
+  end
 end
